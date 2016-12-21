43fedcbbe8ba7e2b8b8fb705bed53c1133e74e67
nemanja-m <nemanja_milicevic@hotmail.com>
2016-12-21 16:50:19 +0100

    Move to elixir 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AlpineRelease.Mixfile do
   def project do
     [app: :alpine_release,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
