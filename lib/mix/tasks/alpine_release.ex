6e379a87786ad1ede1f3279ace663637915b5d5f
Milica Nerlovic <milica.nerlovic@gmail.com>
2019-03-08 13:40:26 +0100

    Remove unecessary commands

--- lib/mix/tasks/alpine_release.ex
+++ lib/mix/tasks/alpine_release.ex
@@ -11,8 +11,6 @@ defmodule Mix.Tasks.Alpine.Release do
     execute "(docker stop builder && docker rm builder) || true"
     execute "docker run --name builder -e MIX_ENV=#{System.get_env("MIX_ENV") || "prod"} -w /release -di elixir:1.4"
     execute "docker cp . builder:/release"
-    execute "docker exec -i builder sh -c 'apk add erlang-edoc'"
-    execute "docker exec -i builder sh -c 'apk add erlang-snmp'"
     execute "docker exec -i builder sh -c 'mix deps.get'"
     execute "docker exec -i builder sh -c 'mix compile'"
     execute "docker exec -i builder sh -c 'mix release'"
